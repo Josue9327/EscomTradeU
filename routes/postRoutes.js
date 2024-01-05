24362b538a2713c3fc56b0dc5251ff9a8e65c04e
Josue Nájera <najera.jimenez.josue@gmail.com>
2024-01-04 21:09:15 -0600

    Post Crud

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -9,5 +9,38 @@ function generateUniqueId(req, res, next) {
     next();
 }
 router.post('/publicar', generateUniqueId, uploadPostImage.single('imageUpload'),postControllers.post);
-router.delete('/borrar/:postId', ensureAuthenticated, postControllers.deletePost);
+router.get('/borrar_post/:postId', ensureAuthenticated, postControllers.deletePost);
+router.get('/editar_post/:postId', ensureAuthenticated, (req, res)=>{
+    const imgname = req.user.user_credential_number + '.jpg';
+    const postId = req.params.postId;
+    const post_author = req.user.user_credential_number;
+    // Comprobar si el usuario está en sesión
+        
+    pool.query('SELECT * FROM post WHERE id = ? AND post_author = ?', [postId, post_author], (error, results) => {
+        if (error) {
+            console.error('Error al borrar el post:', error);
+            return res.status(500).json({ error: 'Error interno del servidor' });
+        }
+
+        // Envía una respuesta exitosa
+        res.render('editpost', { img_route: imgname, post: results[0] });
+    });
+});
+router.post('/editar_post/:postId', ensureAuthenticated, (req, res)=>{
+    const postId = req.params.postId;
+    const post_description = req.body.postContent;
+    const post_author = req.user.user_credential_number;
+    // Comprobar si el usuario está en sesión
+        
+    pool.query('UPDATE post SET post_description = ? WHERE post_author = ? AND id = ?;',
+     [post_description, post_author, postId], (error, results) => {
+        if (error) {
+            console.error('Error al borrar el post:', error);
+            return res.status(500).json({ error: 'Error interno del servidor' });
+        }
+
+        // Envía una respuesta exitosa
+        res.redirect("/mispost");
+    });
+});
 export default router;
